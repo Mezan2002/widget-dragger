759af601e89cc053963f33df3256ac08690d5a8b
mezan2002 <mezanurr009@gmail.com>
2026-01-03 02:18:22 +0600

    feat: initialize React project with Vite, Tailwind CSS, theming, and basic UI components."

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h3>Drag and drop widgets</h3>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
